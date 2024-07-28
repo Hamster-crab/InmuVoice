30f81957e786ae44361322dca7c59895a8598057
Kitano-Hideaki <121480505+Hamster-crab@users.noreply.github.com>
2024-07-28 18:44:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// function example() :void {
+//     const audioElement = document.getElementById('btn_audio') as HTMLAudioElement;
+//     audioElement.currentTime = 0; //連続クリックに対応
+//     audioElement.play(); //クリックしたら音を再生
+// }
+function YJYarimasune() {
+    // 乱数生成
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    var Random = getRandomInt(2);
+    // Main
+    if (Random >= 1) {
+        var audioElement = document.getElementById('YJYarimasune_audio');
+        audioElement.currentTime = 0; //連続クリックに対応
+        audioElement.play(); //クリックしたら音を再生
+    }
+    else {
+        var audioElement = document.getElementById('YJThis_audio');
+        audioElement.currentTime = 0; //連続クリックに対応
+        audioElement.play(); //クリックしたら音を再生
+    }
+}
+function KMRWhyDoYouNeedToSeeIt() {
+    var audioElement = document.getElementById('KMRWhyDoYouNeedToSeeIt_audio');
+    audioElement.currentTime = 0; //連続クリックに対応
+    audioElement.play(); //クリックしたら音を再生
+}
+function MURYes() {
+    var audioElement = document.getElementById('MURYes');
+    audioElement.currentTime = 0; //連続クリックに対応
+    audioElement.play(); //クリックしたら音を再生
+}
---
+++ script.ts
@@ -0,0 +1,36 @@
+// function example() :void {
+//     const audioElement = document.getElementById('btn_audio') as HTMLAudioElement;
+//     audioElement.currentTime = 0; //連続クリックに対応
+//     audioElement.play(); //クリックしたら音を再生
+// }
+
+function YJYarimasune() :void {
+    // 乱数生成
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    let Random = getRandomInt(2);
+
+    // Main
+    if (Random >= 1) {
+    const audioElement = document.getElementById('YJYarimasune_audio') as HTMLAudioElement;
+    audioElement.currentTime = 0; //連続クリックに対応
+    audioElement.play(); //クリックしたら音を再生
+    } else {
+        const audioElement = document.getElementById('YJThis_audio') as HTMLAudioElement;
+        audioElement.currentTime = 0; //連続クリックに対応
+        audioElement.play(); //クリックしたら音を再生
+    }
+}
+
+function KMRWhyDoYouNeedToSeeIt() :void {
+    const audioElement = document.getElementById('KMRWhyDoYouNeedToSeeIt_audio') as HTMLAudioElement;
+    audioElement.currentTime = 0; //連続クリックに対応
+    audioElement.play(); //クリックしたら音を再生
+}
+
+function MURYes() :void {
+    const audioElement = document.getElementById('MURYes') as HTMLAudioElement;
+    audioElement.currentTime = 0; //連続クリックに対応
+    audioElement.play(); //クリックしたら音を再生
+}
